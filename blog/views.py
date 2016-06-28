ddee61a7780c322b4092e3a0e516c5c4b89e0e31
faraz <faraz.dipsite@gmail.com>
2016-06-29 01:06:01 +0530

    added view and template for details

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.core import serializers
@@ -11,4 +11,8 @@ from django.http import HttpResponse
 def post_list(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
+	
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})	
 	
